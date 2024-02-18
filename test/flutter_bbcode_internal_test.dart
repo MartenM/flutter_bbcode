f064757a8556f6b1fcaf56d700ca231cc4da2af4
MartenM <marten.struijk@gmail.com>
2024-02-18 20:41:00 +0100

    Added tests

---
+++ test/flutter_bbcode_internal_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bbcode/flutter_bbcode.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+Widget createTestWidget(Widget child) {
+  return MaterialApp(
+    home: Scaffold(
+      body: child,
+    ),
+  );
+}
+
+void main() {
+  group("BBStylesheet tests", () {
+    test("Copy test (no new variables)", () {
+      var initial = defaultBBStylesheet();
+      var copy = initial.copyWith();
+
+      expect(initial.tags, copy.tags);
+      expect(initial.defaultTextStyle, copy.defaultTextStyle);
+      expect(initial.selectableText, copy.selectableText);
+    });
+
+    test("Copy test (all new variables)", () {
+      var newStyle = TextStyle(fontSize: 999);
+
+      var initial = defaultBBStylesheet();
+      var copy = initial.copyWith(
+        defaultTextStyle: newStyle,
+        selectableText: true
+      );
+
+      expect(initial.tags, copy.tags);
+      expect(copy.defaultTextStyle, newStyle);
+      expect(copy.selectableText, true);
+    });
+
+
+  });
+
+
+}
