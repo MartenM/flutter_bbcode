510e86910853973c75f247f77b15ea8940d526c3
MartenM <marten.struijk@gmail.com>
2024-02-18 20:46:34 +0100

    Fixed analysis issues

--- test/flutter_bbcode_internal_test.dart
+++ test/flutter_bbcode_internal_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bbcode/flutter_bbcode.dart';
 import 'package:flutter_test/flutter_test.dart';
